5471894446b2a941a828860a50c7953fbcbab317
SirCen <52220166+SirCen@users.noreply.github.com>
2019-10-31 12:40:13 +0000

    wip on commands

--- bot.js
+++ bot.js
@@ -1,10 +1,11 @@
 const Discord = require('discord.js');
-var logger = require('winston');
+const logger = require('winston');
 const ytdl = require('ytdl-core');
 const queue = new Map();
 const { prefix, token } = require('./config.json');
 const botID = "608365015610949661";
 var commandUsed = false;
+
 
 // Configure logger settings
 logger.remove(logger.transports.Console);
@@ -18,15 +19,14 @@ client.on('ready', function (evt) {
     logger.info('Connected');
     logger.info('Logged in as: ');
     logger.info(client.username + ' - (' + client.id + ')');
-    //client.setPresence({game : { name : 'with rope'}, status : 'online'});
+    client.user.setActivity('Fromis_9', { type: 'LISTENING' });
 });
-
-// client.on('guildMemberAdd', guildMember => {
-//  console.log('User ' + guildMember.username + ' has joined the server!');
-//
-//  //var newRole = guildMember.guild.roles('name','Test');
-//  guildMember.guild.add.roles('Test');
-// });
+//add role on join
+client.on('guildMemberAdd', guildMember => {
+ var newRole = guildMember.guild.roles.find(role => role.name === 'Test');
+ guildMember.addRole(newRole);
+});
+//ping
 client.on('message', message => {
   if(commandUsed && message.author.bot){
     commandUsed = true;
@@ -37,59 +37,90 @@ client.on('message', message => {
     }
   }
 });
+//Member Count
 client.on('message', message => {
   if(commandUsed && message.author.bot){
-    commandUsed = true;
     return;
   }else {
     if (message.content === `${prefix}members`) {
       message.channel.send(`Total Member Count: ${message.guild.memberCount}`);
     }
+    commandUsed = true;
   }
 });
+//Im and Yurr response
 client.on('message', message => {
-  if (commandUsed && message.author.bot) {
-    commandUsed = true;
-    return;
-  }else {
-    var stringArray = message.content.split(' ');
-    var found;
-    var foundAt;
-    var newMessage = " ";
-    var length = stringArray.length;
-    for(let i = 0; i < length; i++) {
-      if (stringArray[i] === "I'm" | stringArray[i] === "Im" | stringArray[i] === "im") {
-        foundAt = i;
-        found = true;
-        for (let i = foundAt; length > i+1; i++) {
-          newMessage = newMessage + stringArray[i+1] + " ";
+let str = message.content;
+let n = str.search("!hot");
+  if (n > -1) {
+    return commandUsed = true;
+  } else {
+    if (commandUsed && message.author.bot) {
+      return;
+    }else {
+      commandUsed = true;
+      var stringArray = message.content.split(' ');
+      var found;
+      var foundAt;
+      var newMessage = " ";
+      var length = stringArray.length;
+      for(let i = 0; i < length; i++) {
+        if (stringArray[i] === "I'm" | stringArray[i] === "Im" | stringArray[i] === "im") {
+          foundAt = i;
+          found = true;
+          for (let i = foundAt; length > i+1; i++) {
+            newMessage = newMessage + stringArray[i+1] + " ";
+          }
+          newMessage = " " + newMessage.trim();
+          if (found && newMessage !== " Pub Bot") {
+            return message.channel.send("Hi" + newMessage + ", I'm Pub Bot");
+          }
+        } else if (!found) {
+          if (stringArray[i] === "yurr" | stringArray[i] === "Yurr") {
+            console.log(stringArray[i]);
+            if (!stringArray[i] == '!hot') {
+              return;
+            }
+            return message.channel.send('I agree with the above statement');
+          }
         }
-        newMessage = " " + newMessage.trim();
-        if (found && newMessage !== " Pub Bot") {
-          message.channel.send("Hi" + newMessage + ", I'm Pub Bot");
-        }
-      } else if (!found && (stringArray[i] === "yurr" | stringArray[i] === "Yurr")) {
-        message.channel.send('I agree with the above statement');
       }
     }
   }
 });
-
+//max
 client.on('message', message => {
   if(commandUsed && message.author.bot){
     commandUsed = true;
     return;
   }else {
     if (message.content === `${prefix}max`) {
-      message.channel.send('시간이');
+      return message.channel.send('시간이');
     }
   }
 });
-
+//8ball
+client.on('message', message => {
+  var rand = Math.floor(Math.random()*20);
+  const ballAnswers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes, definitely", "You may rely on it",
+                    "As I see it, yes", "Most likely", "Outlook good", "Yes", "All signs point to yes",
+                    "Reply hazy, try again", "Ask again later", "Better not tell you now", "Cannot predict now",
+                    "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no",
+                    "Outlook not so good", "Very doubtful"];
+  if (commandUsed && message.author.bot) {
+    commandUsed = true;
+    return;
+  } else {
+    if (message.content.substring(0,6) === `${prefix}8ball`) {
+      var newMessage = ballAnswers[rand];
+      message.channel.send(newMessage);
+    }
+  }
+});
+//hot
 client.on('message', message => {
   if(!commandUsed && message.author.bot){
     commandUsed = true;
-    console.log('here');
     return;
   }else {
     const args = message.content.slice(prefix.length).split(' ');
@@ -98,9 +129,9 @@ client.on('message', message => {
       var newMessage = message.content.slice(4);
       var rand = Math.floor(Math.random()*2);
       if (rand === 0) {
-        message.channel.send(newMessage + ' is HOT!');
+        return message.channel.send(newMessage + ' is HOT!');
       }else if (rand === 1) {
-       message.channel.send(newMessage + ' is NOT HOT!');
+        return message.channel.send(newMessage + ' is NOT HOT!');
       }
     }
   }
