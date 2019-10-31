f641ff5d1f1ff4a1c9a130f682322d764c9b1c1a
SirCen <52220166+SirCen@users.noreply.github.com>
2019-10-31 02:08:06 +0000

    started transition to new library

--- bot.js
+++ bot.js
@@ -1,9 +1,8 @@
-var Discord = require('discord.io');
+const Discord = require('discord.js');
 var logger = require('winston');
-var auth = require('./auth.json');
 const ytdl = require('ytdl-core');
 const queue = new Map();
-const prefix = "!";
+const { prefix, token } = require('./config.json');
 const botID = "608365015610949661";
 var commandUsed = false;
 
@@ -14,141 +13,218 @@ logger.add(new logger.transports.Console, {
 });
 logger.level = 'debug';
 // Initialize Discord Bot
-var client = new Discord.Client({
-   token: auth.token,
-   autorun: true
-});
+var client = new Discord.Client();
 client.on('ready', function (evt) {
     logger.info('Connected');
     logger.info('Logged in as: ');
     logger.info(client.username + ' - (' + client.id + ')');
-    client.setPresence({game : { name : 'with rope'}, status : 'online'});
+    //client.setPresence({game : { name : 'with rope'}, status : 'online'});
 });
 
-client.on('guildMemberAdd', guildMember => {
- console.log('User ' + guildMember.username + ' has joined the server!');
-
- //var newRole = guildMember.guild.roles('name','Test');
- guildMember.addRole('Test');
-});
-client.on('message', function (user, userID, channelID, message, evt, guild, member) {
-    // Our bot needs to know if it will execute a command
-    commandUsed = false;
-    if(userID != botID && message.substring(0, 1) == prefix && !commandUsed) {
-    // It will listen for messages that will start with `!`
-      var args = message.substring(1).split(' ');
-      var cmd = args[0];
-      args = args.splice(1);
-      switch(cmd) {
-      // Just add any case commands if you want to..
-      // !ping
-        case 'ping':
-          client.sendMessage({
-            to: channelID,
-            message: 'Pong!'
-          });
-        break;
-     //!hot
-        case 'hot':
-          var rand = Math.floor(Math.random()*2);
-          if (rand == 0) {
-            client.sendMessage({
-              to: channelID,
-              message: message.substring(4) + ' is HOT!'
-            });
-          }else if (rand == 1) {
-            client.sendMessage({
-              to: channelID,
-              message: message.substring(4) + ' is NOT HOT!'
-            });
-        }
-        break;
-        //sam
-        case 'sam':
-          client.sendMessage({
-            to: channelID,
-            message: 'A good kid :))'
-          });
-        break;
-       //owen
-        case 'owen':
-          client.sendMessage({
-            to: channelID,
-            message: 'Short'
-          });
-        break;
-      //max
-        case 'max':
-          client.sendMessage({
-            to: channelID,
-            message: '시간이'
-          });
-        break;
-        case 'chris':
-          client.sendMessage({
-            to: channelID,
-            message: "My great creator"
-          });
-        break;
-        case 'cody':
-          client.sendMessage({
-            to: channelID,
-            message: "magine bein a bitch :))\ncould not be me :))"
-          });
-        break;
-        case '8ball':
-          var rand = Math.floor(Math.random()*20);
-          var ballAnswers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes, definitely", "You may rely on it",
-                            "As I see it, yes", "Most likely", "Outlook good", "Yes", "All signs point to yes",
-                            "Reply hazy, try again", "Ask again later", "Better not tell you now", "Cannot predict now",
-                            "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no",
-                            "Outlook not so good", "Very doubtful"];
-          client.sendMessage({
-            to: channelID,
-            message: ballAnswers[rand]
-          });
-        break;
-    //default statement, if command is incorrect
-        default:
-          client.sendMessage({
-            to: channelID,
-            message: 'That is not a valid command, please try again :))'
-          });
-        break;
-      }
-    }else {
-      commandUsed = true;
-    }
-});
-client.on('message', function (user, userID, channelID, message, evt) {
-  var stringArray = message.split(' ');
-  var found;
-  var foundAt;
-  var newMessage = " ";
-  var length = stringArray.length;
-  if(userID != botID && commandUsed && message != newMessage) {
-    for(let i = 0; i < length; i++) {
-      if(stringArray[i] === "I'm" | stringArray[i] === "Im" | stringArray[i] === "im") {
-        foundAt = i;
-        found = true;
-      } else if (!found && (stringArray[i] === "yurr" | stringArray[i] === "Yurr")) {
-        client.sendMessage ({
-          to: channelID,
-          message: "I agree with the above statement"
-        });
-      }
-    }
-    for (let i = foundAt; length > i+1; i++) {
-      newMessage = newMessage + stringArray[i+1] + " ";
-    }
-    newMessage = " " + newMessage.trim();
-    if(found && newMessage !== " Pub Bot") {
-      client.sendMessage({
-        to: channelID,
-        message: "Hi" + newMessage + ", I'm Pub Bot"
-      });
-    }
-  }else {
+// client.on('guildMemberAdd', guildMember => {
+//  console.log('User ' + guildMember.username + ' has joined the server!');
+//
+//  //var newRole = guildMember.guild.roles('name','Test');
+//  guildMember.guild.add.roles('Test');
+// });
+client.on('message', message => {
+  if(commandUsed && message.author.bot){
     commandUsed = true;
+    return;
+  }else {
+    if (message.content === `${prefix}ping`) {
+      message.channel.send('Pong!');
+    }
   }
 });
+client.on('message', message => {
+  if(commandUsed && message.author.bot){
+    commandUsed = true;
+    return;
+  }else {
+    if (message.content === `${prefix}members`) {
+      message.channel.send(`Total Member Count: ${message.guild.memberCount}`);
+    }
+  }
+});
+client.on('message', message => {
+  if (commandUsed && message.author.bot) {
+    commandUsed = true;
+    return;
+  }else {
+    var stringArray = message.content.split(' ');
+    var found;
+    var foundAt;
+    var newMessage = " ";
+    var length = stringArray.length;
+    for(let i = 0; i < length; i++) {
+      if (stringArray[i] === "I'm" | stringArray[i] === "Im" | stringArray[i] === "im") {
+        foundAt = i;
+        found = true;
+        for (let i = foundAt; length > i+1; i++) {
+          newMessage = newMessage + stringArray[i+1] + " ";
+        }
+        newMessage = " " + newMessage.trim();
+        if (found && newMessage !== " Pub Bot") {
+          message.channel.send("Hi" + newMessage + ", I'm Pub Bot");
+        }
+      } else if (!found && (stringArray[i] === "yurr" | stringArray[i] === "Yurr")) {
+        message.channel.send('I agree with the above statement');
+      }
+    }
+  }
+});
+
+client.on('message', message => {
+  if(commandUsed && message.author.bot){
+    commandUsed = true;
+    return;
+  }else {
+    if (message.content === `${prefix}max`) {
+      message.channel.send('시간이');
+    }
+  }
+});
+
+client.on('message', message => {
+  if(!commandUsed && message.author.bot){
+    commandUsed = true;
+    console.log('here');
+    return;
+  }else {
+    const args = message.content.slice(prefix.length).split(' ');
+    const command = args.shift().toLowerCase();
+    if (command === `hot`) {
+      var newMessage = message.content.slice(4);
+      var rand = Math.floor(Math.random()*2);
+      if (rand === 0) {
+        message.channel.send(newMessage + ' is HOT!');
+      }else if (rand === 1) {
+       message.channel.send(newMessage + ' is NOT HOT!');
+      }
+    }
+  }
+  commandUsed = true;
+});
+// client.on('message', function (user, userID, channelID, message, evt, guild, member) {
+//     // Our bot needs to know if it will execute a command
+//     commandUsed = false;
+//     if(userID != botID && message.substring(0, 1) == prefix && !commandUsed) {
+//     // It will listen for messages that will start with `!`
+//       var args = message.substring(1).split(' ');
+//       var cmd = args[0];
+//       args = args.splice(1);
+//       switch(cmd) {
+//       // Just add any case commands if you want to..
+//       // !ping
+//         // case 'ping':
+//         //   client.sendMessage({
+//         //     to: channelID,
+//         //     message: 'Pong!'
+//         //   });
+//         // break;
+//      //!hot
+//         case 'hot':
+//           var rand = Math.floor(Math.random()*2);
+//           if (rand == 0) {
+//             client.sendMessage({
+//               to: channelID,
+//               message: message.substring(4) + ' is HOT!'
+//             });
+//           }else if (rand == 1) {
+//             client.sendMessage({
+//               to: channelID,
+//               message: message.substring(4) + ' is NOT HOT!'
+//             });
+//         }
+//         break;
+//         //sam
+//         case 'sam':
+//           client.sendMessage({
+//             to: channelID,
+//             message: 'A good kid :))'
+//           });
+//         break;
+//        //owen
+//         case 'owen':
+//           client.sendMessage({
+//             to: channelID,
+//             message: 'Short'
+//           });
+//         break;
+//       //max
+//         case 'max':
+//           client.sendMessage({
+//             to: channelID,
+//             message: '시간이'
+//           });
+//         break;
+//         case 'chris':
+//           client.sendMessage({
+//             to: channelID,
+//             message: "My great creator"
+//           });
+//         break;
+//         case 'cody':
+//           client.sendMessage({
+//             to: channelID,
+//             message: "magine bein a bitch :))\ncould not be me :))"
+//           });
+//         break;
+//         case '8ball':
+//           var rand = Math.floor(Math.random()*20);
+//           var ballAnswers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes, definitely", "You may rely on it",
+//                             "As I see it, yes", "Most likely", "Outlook good", "Yes", "All signs point to yes",
+//                             "Reply hazy, try again", "Ask again later", "Better not tell you now", "Cannot predict now",
+//                             "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no",
+//                             "Outlook not so good", "Very doubtful"];
+//           client.sendMessage({
+//             to: channelID,
+//             message: ballAnswers[rand]
+//           });
+//         break;
+//     //default statement, if command is incorrect
+//         default:
+//           client.sendMessage({
+//             to: channelID,
+//             message: 'That is not a valid command, please try again :))'
+//           });
+//         break;
+//       }
+//     }else {
+//       commandUsed = true;
+//     }
+// });
+// client.on('message', function (user, userID, channelID, message, evt) {
+//   var stringArray = message.split(' ');
+//   var found;
+//   var foundAt;
+//   var newMessage = " ";
+//   var length = stringArray.length;
+//   if(userID != botID && commandUsed && message != newMessage) {
+//     for(let i = 0; i < length; i++) {
+//       if(stringArray[i] === "I'm" | stringArray[i] === "Im" | stringArray[i] === "im") {
+//         foundAt = i;
+//         found = true;
+//       } else if (!found && (stringArray[i] === "yurr" | stringArray[i] === "Yurr")) {
+//         client.sendMessage ({
+//           to: channelID,
+//           message: "I agree with the above statement"
+//         });
+//       }
+//     }
+//     for (let i = foundAt; length > i+1; i++) {
+//       newMessage = newMessage + stringArray[i+1] + " ";
+//     }
+//     newMessage = " " + newMessage.trim();
+//     if(found && newMessage !== " Pub Bot") {
+//       client.sendMessage({
+//         to: channelID,
+//         message: "Hi" + newMessage + ", I'm Pub Bot"
+//       });
+//     }
+//   }else {
+//     commandUsed = true;
+//   }
+// });
+client.login(token);
