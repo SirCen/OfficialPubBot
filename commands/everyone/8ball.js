08d2c80e6ca583387813e0a0fa3f4336af5127f6
Chris Wilsher <christopher.wilsher.2018@live.rhul.ac.uk>
2020-06-15 00:55:59 +0100

    Altered 8ball command to be pretty

--- commands/everyone/8ball.js
+++ commands/everyone/8ball.js
@@ -10,7 +10,24 @@ module.exports = {
                       "Reply hazy, try again", "Ask again later", "Better not tell you now", "Cannot predict now",
                       "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no",
                       "Outlook not so good", "Very doubtful"];
-    var newMessage = ballAnswers[rand];
-    message.channel.send(newMessage);
+    const data = [];
+    let question = "";
+    for(const a of args) {
+      question += a + " ";
+    }
+    return message.channel.send({embed: {
+      color: 10761451,
+      title: "Magic 8Ball",
+      fields: [{
+        name: `**` + message.author.username + ` Asked: **`,
+        value: question,
+        },
+        {
+        name: `**Magic 8Ball says: **`,
+        value: ballAnswers[rand],
+        }
+      ]
+    }
+    });
   }
-};
+}
